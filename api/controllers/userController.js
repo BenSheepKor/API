45d1f8754f37ce7f58278494e99ea4866ec42d63
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-12 18:37:50 +0300

    :recycle: var are now const

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -233,7 +233,7 @@ exports.login = async (req) => {
  */
 function validateEmail(email) {
     // taken from https://emailregex.com/
-    var emailRegex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return emailRegex.test(String(email).toLowerCase());
 }
