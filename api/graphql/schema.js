e3499f2a0193e1a084ade0a81aa2376021709439
DimosPagakis <dimosthenispagakis@gmail.com>
2020-04-12 18:36:26 +0300

    :art: Break down graphql schema

--- api/graphql/schema.js
+++ api/graphql/schema.js
@@ -1,51 +1,61 @@
 // Include buildSchema from graphql package
-var { buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
+
+const types = `
+type User {
+  id: Int!,
+  name: String,
+  email: String!,
+  username: String,
+  faculty: String!,
+  lat: String,
+  lng: String,
+},
+type JWT {
+  token: String!
+},
+type Weather {
+  temp: Float!,
+  name: String,
+  description: String,
+  city: String,
+  lat: Float,
+  lng: Float,
+  timestamp: Int
+},
+type Course {
+  name: String!,
+  schedule: [CourseSchedule],
+  professor: String,
+  semester: Int,
+  grade: Float,
+},
+type CourseSchedule{
+  day: Int!,
+  start: Int!,
+  end: Int!,
+}`;
+
+const queries = `
+type Query {
+  users: [User]
+  me: User
+  weather(lat: Float!, lng: Float!): Weather,
+  courses(userId: Int!): [Course]
+},
+`;
+
+const mutations = `
+type Mutation {
+  register(email: String!, password: String!): User,
+  login(email: String, username: String, password: String!): JWT
+},`;
 
 // Define the schema. In order to make sense of what is going on please see https://graphql.org/learn/schema/
-var schema = buildSchema(`
-    type Query {
-        users: [User]
-        me: User
-        weather(lat: Float!, lng: Float!): Weather,
-        courses(userId: Int!): [Course]
-    },
-    type User {
-      id: Int!,
-      name: String,
-      email: String!,
-      username: String,
-      faculty: String!,
-      lat: String,
-      lng: String,
-    },
-    type JWT {
-      token: String!
-    },
-    type Weather {
-      temp: Float!,
-      name: String,
-      description: String,
-      city: String,
-      lat: Float,
-      lng: Float,
-      timestamp: Int
-    },
-    type Course {
-      name: String!,
-      schedule: [CourseSchedule],
-      professor: String,
-      semester: Int,
-      grade: Float,
-    },
-    type CourseSchedule{
-      day: Int!,
-      start: Int!,
-      end: Int!,
-    },
-    type Mutation {
-      register(email: String!, password: String!): User,
-      login(email: String, username: String, password: String!): JWT
-    }
+const schema = buildSchema(`
+    ${types}
+    ${queries}
+    ${mutations}
 `);
 
 // export schema
